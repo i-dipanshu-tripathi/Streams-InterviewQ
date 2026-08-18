e7621b2f75321ca1e87d2035e9e713e012d6190e
i-dipanshu-tripathi <i.dipanshu.tripathi@gmail.com>
2026-08-18 19:13:52 +0530

    Task : Even Numbers from List Date : 18 August 2026

---
+++ src/main/java/org/streams/InterviewQ001/InterviewQ001Solution.java
@@ -0,0 +1,14 @@
+package org.streams.InterviewQ001;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InterviewQ001Solution {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(10, 15, 20, 25, 30, 33, 40);
+        List<Integer> collect = list.stream()
+                .filter(x->x%2==0)
+                .collect(Collectors.toList());
+        System.out.println(collect);
+    }
+}
