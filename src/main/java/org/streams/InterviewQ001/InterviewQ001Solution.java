3a25043a4bc818ab765f35a00210d56450b504a1
i-dipanshu-tripathi <i.dipanshu.tripathi@gmail.com>
2026-08-22 16:34:15 +0530

    Task : Find Even Numbers - Date : 22 August 2026

--- src/main/java/org/streams/InterviewQ001/InterviewQ001Solution.java
+++ src/main/java/org/streams/InterviewQ001/InterviewQ001Solution.java
@@ -6,9 +6,9 @@ import java.util.stream.Collectors;
 public class InterviewQ001Solution {
     public static void main(String[] args) {
         List<Integer> list = List.of(10, 15, 20, 25, 30, 33, 40);
-        List<Integer> collect = list.stream()
+        List<Integer> evenNumberList = list.stream()
                 .filter(x->x%2==0)
                 .collect(Collectors.toList());
-        System.out.println(collect);
+        System.out.println(evenNumberList);
     }
 }
