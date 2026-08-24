caa628904f465cff2974f4bb8471cc3d1ff90f9b
i-dipanshu-tripathi <i.dipanshu.tripathi@gmail.com>
2026-08-25 00:10:23 +0530

    Task : Maximum and Minimum Date : 25 August 2026

---
+++ src/main/java/org/streams/InterviewQ004/InterviewQ004Solution.java
@@ -0,0 +1,40 @@
+package org.streams.InterviewQ004;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InterviewQ004Solution {
+    public static void main(String[] args) {
+        List<Integer> integerList = List.of(10, 25, -5, 40, 15, 30, 8);
+
+        Integer max1 = integerList.stream()
+                                  .reduce(Integer.MIN_VALUE,Integer::max);
+
+        Integer min1 = integerList.stream()
+                                  .reduce(Integer.MAX_VALUE,Integer::min);
+
+        System.out.println("Min and Max using reduce : "+min1+" "+max1);
+
+        int max2 = integerList.stream()
+                                .mapToInt(Integer::intValue)
+                                .max()
+                                .orElse(Integer.MIN_VALUE);
+
+        int min2 = integerList.stream()
+                              .mapToInt(Integer::intValue)
+                              .min()
+                              .orElse(Integer.MAX_VALUE);
+
+        System.out.println("Min and Max using reduce : "+min2+" "+max2);
+
+        int max3 = integerList.stream()
+                              .collect(Collectors.summarizingInt(Integer::intValue))
+                              .getMax();
+
+        int min3 = integerList.stream()
+                .collect(Collectors.summarizingInt(Integer::intValue))
+                .getMin();
+
+        System.out.println("Min and Max using summaringInt method : "+min3+" "+max3);
+    }
+}
