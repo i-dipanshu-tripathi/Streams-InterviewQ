59dd382bcb933fdc8f5d710f1ed076f37345d5bd
i-dipanshu-tripathi <i.dipanshu.tripathi@gmail.com>
2026-08-25 00:34:16 +0530

    Task : Capitialize the words Date : 25 August 2026

--- src/main/java/org/streams/InterviewQ006/InterviewQ006Solution.java
+++ src/main/java/org/streams/InterviewQ006/InterviewQ006Solution.java
@@ -8,10 +8,16 @@ public class InterviewQ006Solution {
     public static void main(String[] args) {
         List<String> names = List.of("john", "Alice", "bob", "JOHN", "pRiYa");
 
-        List<String> namesInCaptialList1 = names.stream().map(str->str.toUpperCase()).collect(Collectors.toList());
+        List<String> namesInCaptialList1 = names.stream()
+                                                .map(str->str.toUpperCase())
+                                                .collect(Collectors.toList());
+
         System.out.println(namesInCaptialList1);
 
-        List<String> namesInCapitalList2 = names.stream().map(String::toUpperCase).collect(Collectors.toList());
+        List<String> namesInCapitalList2 = names.stream()
+                                                .map(String::toUpperCase)
+                                                .collect(Collectors.toList());
+
         System.out.println(namesInCapitalList2);
     }
 }
